9df859acbf89be67658601edb00c084f42c65fde
TheSeed <735708067@qq.com>
2016-04-09 22:18:41 +0800

    学习慕课学院的制作，同时增加双人游戏模式

---
+++ New Unity Project/Assets/Script/Title.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Title : MonoBehaviour {
+
+    public Button StartButton;
+    public Button ExitButton;
+    public Button DoubleButton;
+    // board窗口
+    public GameObject board;
+    public MainLoop loop;
+   
+	// Use this for initialization
+	void Start () {
+        StartButton.onClick.AddListener(() =>
+        {
+            gameObject.SetActive(false);
+            board.SetActive(true);
+        });
+
+        ExitButton.onClick.AddListener(() =>
+        {
+            Application.Quit();
+        });
+        DoubleButton.onClick.AddListener(() => {
+            gameObject.SetActive(false);
+            board.SetActive(true);
+            loop.SetTwopersonGameStyle();
+        });
+    }
+	
+}
