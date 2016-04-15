dfdc7dae2cd3a5c9406d8c119dc9fc23592bef92
TheSeed <735708067@qq.com>
2016-04-15 23:05:09 +0800

    UGUI自我学习第一季，加油！

---
+++ UGIstudy/Assets/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+ /*  public void OnStartGame(string sceneName) {
+        Application.LoadLevel(sceneName);
+    }
+    */
+    public void OnStartGame(int Sceneindex) {
+        Application.LoadLevel(Sceneindex);
+    }
+}
---
+++ UGIstudy/Assets/Player.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 90;
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * speed);//Vector3.up
+	}
+    public void changspeed(float newspeed) {
+        this.speed = newspeed;
+    }
+}
