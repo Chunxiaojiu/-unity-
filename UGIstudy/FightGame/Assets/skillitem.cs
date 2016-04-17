c92769c60a2bde5e37d1cf48449e6a4fb32ee345
TheSeed <735708067@qq.com>
2016-04-17 21:42:48 +0800

    UGUI学习代码项目上传2

---
+++ UGIstudy/FightGame/Assets/skillitem.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class skillitem : MonoBehaviour {
+    public float coldtime = 2;
+    private float time = 0;
+    private Image fillimage;
+    private bool isStarttime = false;
+	// Use this for initialization
+	void Start () {
+        fillimage = transform.Find("fillimage").GetComponent<Image>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isStarttime) {
+            time += Time.deltaTime;
+            fillimage.fillAmount = (coldtime - time) / coldtime;
+            if (time >= coldtime) {
+                isStarttime = false;
+                time = 0;
+                fillimage.fillAmount = 0;
+            }
+        }
+	}
+    public void Onclick() {
+        isStarttime = true;
+    }
+}
